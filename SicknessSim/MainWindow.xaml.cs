0269777741a841e8381f965c62a0cd4ec40b0c86
Martin Tomasi <martin.tomasi@gmail.com>
2014-11-15 20:17:38 +0100

    Initial Commit

---
+++ SicknessSim/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SicknessSim {
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: Window {
+        private readonly WriteableBitmap bitmap;
+        private readonly Simulation simulation;
+
+        public MainWindow() {
+            InitializeComponent();
+            simulation = new Simulation(Constants.PopulationSize);
+            bitmap = BitmapFactory.New(800, 800);
+            img.Source = bitmap;
+        }
+
+        public void DrawSimulation() {
+            using (bitmap.GetBitmapContext()) {
+                bitmap.Clear();
+                foreach (var person in simulation.Persons) {
+                    var color = Colors.Green;
+
+                    switch (person.Status) {
+                        case Status.Healthy:
+                            color = Constants.HealthyColor;
+                            break;
+                        case Status.Infectious:
+                            color = Constants.InfectiousColor;
+                            break;
+                        case Status.Sick:
+                            color = Constants.SickColor;
+                            break;
+                        case Status.Dead:
+                            color = Constants.DeadColor;
+                            break;
+                        default:
+                            Debug.Fail("uuuhhhh");
+                            break;
+                    }
+                    //var r = (int)Constants.InfluenceRadius;
+                    //bitmap.FillEllipseCentered((int)person.Position.X, (int)person.Position.Y, r, r, Color.FromArgb(80, 255, 0, 0));
+                    bitmap.FillEllipseCentered((int) person.Position.X, (int) person.Position.Y, 2, 2, color);
+                }
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e) {
+            CompositionTarget.Rendering += (s, t) => {
+                DrawSimulation();
+                simulation.Tick();
+            };
+        }
+    }
+}
---
+++ SicknessSim/Vector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SicknessSim {
+    internal struct Vector {
+        public static readonly Vector Zero = new Vector(0, 0);
+
+        public static readonly Vector One = new Vector(1, 1);
+        public readonly double X, Y;
+
+        public Vector(double X, double Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj is Vector) {
+                var p = (Vector) obj;
+                return (p.X == X) && (p.Y == Y);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+
+        public override string ToString() {
+            return "[" + X + ", " + Y + "]";
+        }
+
+        public double Dot(Vector that) {
+            return X * that.X + Y * that.Y;
+        }
+
+        public static Vector operator +(Vector a, Vector b) {
+            return new Vector(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector operator -(Vector a, Vector b) {
+            return new Vector(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static Vector operator *(Vector a, Vector b) {
+            return new Vector(a.X * b.X, a.Y * b.Y);
+        }
+
+        public static Vector operator *(Vector a, double b) {
+            return new Vector(a.X * b, a.Y * b);
+        }
+
+        public static Vector operator +(Vector a, double b) {
+            return new Vector(a.X + b, a.Y + b);
+        }
+
+        public static bool operator ==(Vector a, Vector b) {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Vector a, Vector b) {
+            return !(a.Equals(b));
+        }
+
+        public double DistanceTo(Vector other) {
+            var xx = (X - other.X) * (X * other.X);
+            var yy = (Y - other.Y) * (Y * other.Y);
+            return Math.Sqrt(xx + yy);
+        }
+    }
+}
